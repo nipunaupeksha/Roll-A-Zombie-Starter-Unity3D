59344775b5246e7d860abb0a64234a1b536eb01f
nipunaupeksha <nipuna.upeksha@gmail.com>
2019-10-13 11:49:20 +0530

    zombie-0

---
+++ Assets/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject selectedZombie;
+    public List<GameObject>zombies; 
+    public Vector3 selectedSize;
+    public Vector3 defaultSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SelectZombie(selectedZombie);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SelectZombie(GameObject newZombie){
+        newZombie.transform.localScale = selectedSize;
+    }
+}
